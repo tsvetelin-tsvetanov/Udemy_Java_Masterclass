50c9022133412e01896c2d816b52e03165e90eed
Tsvetelin <tsvetelintsvetanov94@gmail.com>
2020-07-22 10:50:06 -0700

    Inheritance

---
+++ Inheritance/src/com/company/House.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class House {
+    private String color;
+
+    public House(String color){
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
--- Inheritance/src/com/company/Main.java
+++ Inheritance/src/com/company/Main.java
@@ -3,6 +3,32 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
+	    Animal animal = new Animal("Animal", 1, 1, 5, 5);
+
+	    Dog dog = new Dog("Yorkie", 8, 20, 2, 4, 1, 20, "long silky");
+
+	    //dog.eat();
+	    dog.walk();
+	    //dog.run();
+
+		House blueHouse = new House("blue");
+		House anotherHouse = blueHouse;
+
+		System.out.println(blueHouse.getColor());
+		System.out.println(anotherHouse.getColor());
+
+		anotherHouse.setColor("yellow");
+
+		System.out.println(blueHouse.getColor());
+		System.out.println(anotherHouse.getColor());
+
+		System.out.println("*************");
+		House greenHouse = new House("green");
+		anotherHouse = greenHouse;
+		System.out.println(blueHouse.getColor());
+		System.out.println(anotherHouse.getColor());
+		System.out.println(greenHouse.getColor());
+
+
+	}
 }
