a73e5ea71463009d5caec02212becf0e15e3bb77
Tsvetelin <tsvetelintsvetanov94@gmail.com>
2020-07-27 13:24:27 -0700

    Challenge

---
+++ OOPTest/src/com/company/DeluxeBurger.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class DeluxeBurger extends Hamburger{
+    public DeluxeBurger() {
+        super("Deluxe", "Sausage & Bacon", 14.54,
+                "White");
+        super.addHamburgerAddition1("Chips", 2.75);
+        super.addHamburgerAddition2("Drink", 1.81);
+    }
+
+    public DeluxeBurger(String name, String meat, double price, String breadRollType) {
+        super(name, meat, price, breadRollType);
+    }
+
+    @Override
+    public void addHamburgerAddition1(String name, double price) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition2(String name, double price) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition3(String name, double price) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition4(String name, double price) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
+}
--- OOPTest/src/com/company/Main.java
+++ OOPTest/src/com/company/Main.java
@@ -3,6 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+//        Hamburger hamburger = new Hamburger("Basic", "Sausage",
+//                3.56, "White roll");
+//        double price = hamburger.itmemizeHamburger();
+//        hamburger.addHamburgerAddition1("Tomato", 0.27);
+//        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+//        hamburger.addHamburgerAddition3("Cheese", 1.13);
+//        System.out.println("Total burger price is " +
+//                hamburger.itmemizeHamburger());
+//
+//        HealthyBurger healthyBurger = new HealthyBurger("Bacon", 5.67);
+//        healthyBurger.itmemizeHamburger();
+//        healthyBurger.addHealthAddition1("Egg", 5.43);
+//        healthyBurger.addHealthAddition2("Lentils", 3.41);
+//        healthyBurger.itmemizeHamburger();
+//        System.out.println("Total healthy burger price is " +
+//                healthyBurger.itmemizeHamburger());
+
+        DeluxeBurger db = new DeluxeBurger();
+        db.addHamburgerAddition3("Should not do this", 50.53);
+        System.out.println("Total deluxe burger price is " +
+                db.itmemizeHamburger());
     }
 }
